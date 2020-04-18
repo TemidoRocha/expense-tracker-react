90cc28f31fbfd9156913b8486c7d415c7146f58f
TemidoRocha <manueltemidorocha@gmail.com>
2020-04-18 19:55:08 +0100

    react app completed - front

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import Header from './components/Header/index';
+import Balance from './components/Balance/index';
+import IncomeExpenses from './components/IncomeExpenses/index';
+import TransactionList from './components/TransactionList/index';
+import AddTransaction from './components/AddTransaction/index';
+
+import { GlobalProvider } from './context/GlobalState';
 
 import './App.css';
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    <GlobalProvider>
+      <Header>Hello World</Header>
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </GlobalProvider>
   );
 }
 
